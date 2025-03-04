570f725105851fb2232f671ab267523ca6b370d8
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2025-03-05 00:14:14 +0500

    Made some wierd changes. I might be commiting .env to github as well

--- promptly/app/lib/supabase.js
+++ promptly/app/lib/supabase.js
@@ -1,9 +1,13 @@
-// lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
 export const createSupabaseClient = () => {
   const supabaseUrl = "https://ibffsuyqxmtxfzriuqfc.supabase.co";
-  const supabaseKey = process.env.SUPABASE_KEY;
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZmZzdXlxeG10eGZ6cml1cWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NjEzMzksImV4cCI6MjA1NjMzNzMzOX0.27KF7xaPk8IXg7jhPoS2xRDfWbdYxNQYT3Ub6_R3m2g";
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Missing Supabase environment variables");
+  }
 
   return createClient(supabaseUrl, supabaseKey);
 };
